fa24a322e4fb06ca01e57979a983da31f398d64a
Eugene Asiedu <ngene84@gmail.com>
2015-05-24 16:29:36 +0000

    Several improvements
    Custom error object
    Make the api similar to the SMSGH Java API

--- index.js
+++ index.js
@@ -1,3 +1,33 @@
-exports.TopUp = require('./lib/topup')
-exports.SendMessage = require('./lib/sendmsg')
-exports.Message = require('./lib/message')
+var SMSGHError = require('./lib/error')
+var TopUp = require('./lib/topup')
+var SendMessage = require('./lib/sendmsg')
+var Message = require('./lib/message')
+
+function SMSGH (clientId, clientSecret, apiKey) {
+  if (!(this instanceof SMSGH)) return new SMSGH(clientId, clientSecret, apiKey)
+
+  if (clientId === undefined || clientSecret === undefined) {
+    throw new SMSGHError('Make sure clientid and clientSecret are both defined')
+  }
+
+  this.clientId = clientId
+  this.clientSecret = clientSecret
+  this.apiKey = apiKey
+  this.versionNumber = 'v3'
+  this.messageApi = new SendMessage(clientId, clientSecret)
+  if (this.apiKey) this.topUpApi = new TopUp(apiKey)
+}
+
+SMSGH.prototype.setContextPath = function (versionNumber) {
+  if (typeof versionNumber === 'number') {
+    versionNumber = 'v' + versionNumber
+  }
+
+  this.versionNumber = versionNumber
+}
+
+SMSGH.TopUp = TopUp
+SMSGH.SendMessage = SendMessage
+SMSGH.Message = Message
+
+module.exports = SMSGH
