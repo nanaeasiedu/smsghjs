a73fd8837724e236133dcbcc63e46dc5e53608f6
Eugene Asiedu <ngene84@gmail.com>
2015-06-12 00:10:22 +0000

    fix toJSON method to return JSON object
    add test and travis

---
+++ test/message.js
@@ -0,0 +1,33 @@
+var test = require('tape')
+var Message = require('../lib/message')
+
+test('message object throws exception without important params', function (t) {
+  t.plan(1)
+
+  var opts = {
+    type: 0,
+    time: 'Thu Jun 11 2015 18:16:22'
+  }
+
+  t.throws(function () {
+    Message(opts)
+  })
+})
+
+test('toJSON method test', function (t) {
+  t.plan(1)
+
+  var opts = {
+    from: 'SMSGHJS',
+    to: '+233252271893',
+    content: 'Hello World',
+    type: '0'
+  }
+
+  t.deepEqual(new Message(opts).toJSON(), {
+    From: 'SMSGHJS',
+    To: '+233252271893',
+    Content: 'Hello World',
+    Type: '0'
+  })
+})
---
+++ test/utils.js
@@ -0,0 +1,40 @@
+var test = require('tape')
+var utils = require('../lib/utils')
+
+test('encode test', function (t) {
+  t.plan(1)
+  t.equal(utils.encode('abc'), 'YWJj', 'enocding to base64 works')
+})
+
+test('serialize test', function (t) {
+  t.plan(2)
+
+  var expectedForTrueForAll = 'names=Napoleon%20%26%20Albert%20Einstein&status=dead'
+  var expectedForFalseProps = 'names=Napoleon & Albert Einstein&status=dead'
+  var obj = {
+    names: 'Napoleon & Albert Einstein',
+    status: 'dead'
+  }
+
+  t.equal(utils.serialize(obj, true, true), expectedForTrueForAll, 'serialize props and values both encoded works')
+  t.equal(utils.serialize(obj, true, false), expectedForFalseProps, 'serialize props and values with values not encoded works')
+})
+
+test('merge test', function (t) {
+  t.plan(1)
+
+  var objA = {
+    gender: 'male'
+  }
+
+  var objB = {
+    name: 'Albert Einstein'
+  }
+
+  var mergedObj = {
+    gender: 'male',
+    name: 'Albert Einstein'
+  }
+
+  t.deepEqual(utils.merge(objA, objB), mergedObj, 'merge works')
+})
