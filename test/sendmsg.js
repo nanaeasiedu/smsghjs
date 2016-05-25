9930f7e13f995348248694317b65b2b091929116
Eugene Asiedu <ngene84@gmail.com>
2016-05-25 21:04:47 +0000

    Add an example for send message

---
+++ test/sendmsg.js
@@ -0,0 +1,34 @@
+var test = require('tape')
+var SMSGH = require('../index')
+
+var sms = new SMSGH({
+  clientId: process.env.CLIENT_ID,
+  clientSecret: process.env.CLIENT_SECRET
+})
+
+var Message = SMSGH.Message
+var testMessage = new Message({ from: 'SMSGHJS', to: '233272271893', content: 'Testing SMSGH API WRAPPER LIBRARY' })
+
+sms.setContextPath('v3')
+
+var id = ''
+
+test('Should send sms message', function (t) {
+  t.plan(2)
+
+  sms.messaging.send(testMessage, function (err, res) {
+    t.notOk(err, 'No error message')
+    t.ok(res, 'Message was sent successfully')
+    id = res.MessageId
+  })
+})
+
+
+test('Should send quick sms message using', function (t) {
+  t.plan(2)
+
+  sms.messaging.sendQuickMessage(testMessage, function (err, res) {
+    t.notOk(err, 'No error sending quick message')
+    t.ok(res, 'Quick message was sent successfully')
+  })
+})
