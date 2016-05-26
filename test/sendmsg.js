7ed85e2e646188ca2f72204bef41dada6ad5247e
Eugene Asiedu <ngene84@gmail.com>
2016-05-26 00:49:22 +0000

    Write test for `sendQuickMessage`

--- test/sendmsg.js
+++ test/sendmsg.js
@@ -11,23 +11,19 @@ var testMessage = new Message({ from: 'SMSGHJS', to: '233272271893', content: 'T
 
 sms.setContextPath('v3')
 
-var id = ''
-
 test('Should send sms message', function (t) {
   t.plan(2)
 
   sms.messaging.send(testMessage, function (err, res) {
     t.notOk(err, 'No error message')
     t.ok(res, 'Message was sent successfully')
-    id = res.MessageId
   })
 })
-
 
 test('Should send quick sms message using', function (t) {
   t.plan(2)
 
-  sms.messaging.sendQuickMessage(testMessage, function (err, res) {
+  sms.messaging.sendQuickMessage(testMessage.from, testMessage.to, 'Sending a quick message', function (err, res) {
     t.notOk(err, 'No error sending quick message')
     t.ok(res, 'Quick message was sent successfully')
   })
